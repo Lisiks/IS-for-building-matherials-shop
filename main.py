b57bec37c0ca9a23a570570e0089792a4c2f3a22
MrLisiks <mikhail.shirokov.2007@mail.ru>
2025-09-25 11:17:30 +0300

    Adding simple GUI windows

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import customtkinter as ctk
+
+
+class Application(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.w = self.winfo_screenwidth()
+        self.h = self.winfo_screenheight()
+        self.after(0, lambda: self.state('zoomed'))
+        self.title("Информационная система для строительного магазина")
+
+    def run(self):
+        self.mainloop()
+
+
+my_application = Application()
 if __name__ == '__main__':
-    print("Hello 3 course!!!")
+    my_application.run()
