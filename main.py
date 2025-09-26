6302ecf786592c795370d389d0d49bdc6f5e2c6e
MrLisiks <mikhail.shirokov.2007@mail.ru>
2025-09-26 21:18:43 +0300

    Merge NavigationPanel class with Application class
    * Вложенный класс панели навигации был слит в основной класс приложения
      из за неудобства реализаци взаимодествия класса приложения и
      вложенного класса панели навигации.
    
     * В настоящий момент элементы рамки панели навигации являются
       приватными атрибутами класса приложения, а сама пональ создается
       приветной функцией create_navigation_panel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import customtkinter as ctk
-
-
-CLASSIC_WINDOW_WIDTH = 1980
-CLASSIC_WINDOW_HEIGHT = 1080
+from main_menu_form import MainMenuForm
+from global_const import *
 
 
 class Application(ctk.CTk):
@@ -13,113 +11,113 @@ class Application(ctk.CTk):
         self.after(0, lambda: self.state('zoomed'))
         self.title("Информационная система для строительного магазина")
 
-        self.navigation_panel = self.__NavigationPanel(self, self.w, self.h)
-        self.navigation_panel.grid(row=0, column=0)
+        self.__current_form: ctk.CTkFrame = None
+
+        self.__navigation_panel = self.__create_navigation_panel()
+        self.main_menu_form = MainMenuForm(self)
+
+        self.__navigation_panel.grid(row=0, column=0)
+        self.change_form(self.main_menu_form)
+
+    def change_form(self, new_form: ctk.CTkFrame):
+        if self.__current_form is not None:
+            self.__current_form.forget()
+        self.__current_form = new_form
+        new_form.grid(row=0, column=1, columnspan=3, padx=10, pady=10)
 
     def run(self):
         self.mainloop()
 
-    class __NavigationPanel(ctk.CTkFrame):
-        __CLASSIC_FONT_SIZE = 30
+    def __create_navigation_panel(self) -> ctk.CTkFrame:
+        font_size = int(CLASSIC_NAVIGATION_PANEL_FONT_SIZE * (self.w / CLASSIC_WINDOW_WIDTH))
+        buttons_w = self.w // 4
+        buttons_h = (self.h - 65) // 8
 
-        def __init__(self, master, window_w, window_h):
-            super().__init__(master)
-            self.configure(fg_color="#3F48CC", corner_radius=0, width=window_w)
+        navigation_panel = ctk.CTkFrame(master=self)
 
-            font_size = int(self.__CLASSIC_FONT_SIZE * (window_w / CLASSIC_WINDOW_WIDTH))
-            buttons_h = (window_h - 65) // 9
-            buttons_w = buttons_h * 4
+        self.__navigation_panel = ctk.CTkFrame(master=self)
+        self.__main_menu_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="ИС",
+            font=("Times new roman", font_size + 20),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__main_menu_button = ctk.CTkButton(
-                master=self,
-                text="ИС",
-                font=("Times new roman", font_size + 20),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__nomenclature_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="Номенклатура",
+            font=("Arial", font_size),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__fin_operation_button = ctk.CTkButton(
-                master=self,
-                text="Финансовые операции",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__sales_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="Продажи",
+            font=("Arial", font_size),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__nomenclature_button = ctk.CTkButton(
-                master=self,
-                text="Номенклатура",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__purchases_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="Закупки",
+            font=("Arial", font_size),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__sales_button = ctk.CTkButton(
-                master=self,
-                text="Продажи",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__write_offs_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="Списания",
+            font=("Arial", font_size),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__purchases_button = ctk.CTkButton(
-                master=self,
-                text="Закупки",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__suppliers_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="Поставщики",
+            font=("Arial", font_size),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__write_offs_button = ctk.CTkButton(
-                master=self,
-                text="Списания",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__clients_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="Клиенты",
+            font=("Arial", font_size),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__suppliers_offs_button = ctk.CTkButton(
-                master=self,
-                text="Поставщики",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__reports_button = ctk.CTkButton(
+            master=navigation_panel,
+            text="Отчеты",
+            font=("Arial", font_size),
+            width=buttons_w,
+            height=buttons_h,
+            corner_radius=0,
+        )
 
-            self.__clients_offs_button = ctk.CTkButton(
-                master=self,
-                text="Клиенты",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
+        self.__main_menu_button.grid(row=0, column=0)
+        self.__nomenclature_button.grid(row=1, column=0)
+        self.__sales_button.grid(row=2, column=0)
+        self.__purchases_button.grid(row=3, column=0)
+        self.__write_offs_button.grid(row=4, column=0)
+        self.__suppliers_button.grid(row=5, column=0)
+        self.__clients_button.grid(row=6, column=0)
+        self.__reports_button.grid(row=7, column=0)
 
-            self.__reports_offs_button = ctk.CTkButton(
-                master=self,
-                text="Отчеты",
-                font=("Arial", font_size),
-                width=buttons_w,
-                height=buttons_h,
-                corner_radius=0
-            )
-
-            self.__main_menu_button.grid(row=0, column=0)
-            self.__fin_operation_button.grid(row=1, column=0)
-            self.__nomenclature_button.grid(row=2, column=0)
-            self.__sales_button.grid(row=3, column=0)
-            self.__purchases_button.grid(row=4, column=0)
-            self.__write_offs_button.grid(row=5, column=0)
-            self.__suppliers_offs_button.grid(row=6, column=0)
-            self.__clients_offs_button.grid(row=7, column=0)
-            self.__reports_offs_button.grid(row=8, column=0)
+        return navigation_panel
 
 
 my_application = Application()
