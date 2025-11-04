27f3de397f57957652c820cf7ad10177d0feaff8
MrLisiks <mikhail.shirokov.2007@mail.ru>
2025-11-04 09:44:26 +0300

    Adding validation function for inn, ogrn, email and phone number

---
+++ Back/validators.py
@@ -0,0 +1,23 @@
+def inn_validation(inn_number) -> bool:
+    return inn_number.isdigit() and len(inn_number) == 10
+
+
+def ogrn_validation(ogrn_number) -> bool:
+    return ogrn_number.isdigit() and len(ogrn_number) == 13
+
+
+def email_validation(email) -> bool:
+    return (5 <= len(email) <= 30) and (email.count("@") == 1)
+
+
+def telephone_validation(telephone_number) -> bool:
+    telephone_component_list = telephone_number.strip().split("-")
+    print(telephone_component_list)
+    return (
+            len(telephone_component_list) == 5 and
+            (telephone_component_list[0].isdigit() and 1 <= len(telephone_component_list[0]) <= 3) and
+            (telephone_component_list[1].isdigit() and len(telephone_component_list[1]) == 3) and
+            (telephone_component_list[2].isdigit() and len(telephone_component_list[2]) == 3) and
+            (telephone_component_list[3].isdigit() and len(telephone_component_list[3]) == 2) and
+            (telephone_component_list[4].isdigit() and len(telephone_component_list[4]) == 2)
+    )
