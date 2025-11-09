4e1c8c8d4c05beb697df0768905ae034b65dea94
MrLisiks <mikhail.shirokov.2007@mail.ru>
2025-11-09 18:16:36 +0300

    Adding sales backend

---
+++ Back/backend_for_sales.py
@@ -0,0 +1,90 @@
+from Back.database_connector import get_connector
+from Back.validators import article_validation
+
+
+def get_sales() -> list:
+    connector = get_connector()
+    cursor = connector.cursor()
+
+    selection_query = """SELECT Sales.ID, Sales.SaleDate, ClientSales.Clients_DiscountCardNumber, Sales.Products_ProductArticle, Sales.ProductCount 
+    FROM Sales LEFT JOIN ClientSales ON Sales.ID = ClientSales.Sales_ID;"""
+    cursor.execute(selection_query)
+
+    connector.close()
+    return cursor.fetchall()
+
+
+def add_sale(date, client_card, product_article, product_count) -> int:
+    if not article_validation(product_article):
+        raise TypeError("Incorrect article")
+    if not product_count.isdigit() or int(product_count) < 1:
+        raise TypeError("Incorrect count")
+    if client_card != "" and (not client_card.isdigit() or not len(client_card) == 10):
+        raise TypeError("Incorrect card")
+
+    connector = get_connector()
+    cursor = connector.cursor()
+
+    check_article_query = "SELECT Count FROM Products WHERE ProductArticle = %s;"
+    cursor.execute(check_article_query, (product_article,))
+    query_result = cursor.fetchall()
+    if len(query_result) != 1:
+        raise TypeError("Article doesnt exist")
+    elif query_result[0][0] < int(product_count):
+        raise TypeError("Product count is very big")
+
+    if client_card != "":
+        checking_client = "SELECT count(*) FROM Clients WHERE DiscountCardNumber = %s;"
+        cursor.execute(checking_client, (client_card,))
+        if cursor.fetchall()[0][0] != 1:
+            raise TypeError("Client doesnt exist")
+
+    add_sale_query = """INSERT INTO Sales 
+    (SaleDate, Products_ProductArticle, ProductCount)
+    VALUES(%s, %s, %s)"""
+    cursor.execute(add_sale_query, (date, product_article, int(product_count)))
+    connector.commit()
+
+    cursor.execute("""SELECT LAST_INSERT_ID();""")
+    new_record_id = cursor.fetchall()[0][0]
+
+    if client_card != "":
+        add_sale_client_query = """INSERT INTO ClientSales VALUES(%s, %s)"""
+        cursor.execute(add_sale_client_query, (new_record_id, client_card))
+
+    update_product_count_query = """UPDATE Products SET Count = Count - %s WHERE ProductArticle = %s;"""
+    cursor.execute(update_product_count_query, (product_count, product_article))
+    connector.commit()
+
+    connector.close()
+    return new_record_id
+
+
+def del_sale(sale_id):
+    connector = get_connector()
+    cursor = connector.cursor()
+
+    delete_sale_query = """DELETE FROM Sales WHERE ID = %s"""
+    cursor.execute(delete_sale_query, (sale_id,))
+    connector.commit()
+    connector.close()
+
+
+def get_finding_sales(attribute) -> list:
+    connector = get_connector()
+    cursor = connector.cursor()
+
+    liked_attribute = f"%{attribute}%"
+    if attribute.isdigit():
+        selection_query = """SELECT Sales.ID, Sales.SaleDate, ClientSales.Clients_DiscountCardNumber, Sales.Products_ProductArticle, Sales.ProductCount 
+        FROM Sales LEFT JOIN ClientSales ON Sales.ID = ClientSales.Sales_ID
+        WHERE Sales.SaleDate LIKE %s OR Sales.Products_ProductArticle LIKE %s OR Sales.ProductCount = %s OR ClientSales.Clients_DiscountCardNumber LIKE %s;"""
+        cursor.execute(selection_query, (liked_attribute, liked_attribute, int(attribute), liked_attribute))
+    else:
+        selection_query = """SELECT Sales.ID, Sales.SaleDate, ClientSales.Clients_DiscountCardNumber, Sales.Products_ProductArticle, Sales.ProductCount 
+        FROM Sales LEFT JOIN ClientSales ON Sales.ID = ClientSales.Sales_ID
+        WHERE Sales.SaleDate LIKE %s OR Sales.Products_ProductArticle LIKE %s OR ClientSales.Clients_DiscountCardNumber LIKE %s;"""
+        cursor.execute(selection_query, (liked_attribute, liked_attribute, liked_attribute))
+
+    connector.close()
+    return cursor.fetchall()
--- Front/sales_form.py
+++ Front/sales_form.py
@@ -1,6 +1,13 @@
 import customtkinter as ctk
-import CTkTable
+import mysql.connector.errors
+import Back.backend_for_sales as back
+from tksheet import Sheet
 from Front.global_const import *
+from Back.query_for_comboboxes_values import get_products_articles, get_client_cards
+from Front.dialog_window import InformationDialog, ModalDialog
+from datetime import datetime
+
+
 
 
 class SalesForm(ctk.CTkFrame):
@@ -8,85 +15,114 @@ class SalesForm(ctk.CTkFrame):
         super().__init__(master)
         self.configure(fg_color=master.cget("fg_color"))
 
-        scrollbar_frame_width = window_w - (window_w // 4) - 60
-        scrollbar_frame_height = window_h - (window_h // 2.7) - 60
+        table_width = window_w - (window_w // 4) - 60
+        table_height = window_h - (window_h // 2.7) - 60
 
-        all_table_width = scrollbar_frame_width - 20
-        all_table_height = scrollbar_frame_height - 20
-
-        cell_table_height = all_table_height // 7
-
-        table_width_percentage = all_table_width / 100
+        self.__table_column_width = int(table_width // 4)
+        table_row_height = int(table_height // 7)
 
         head_font_size = round(CLASSIC_HEAD_FONT_SIZE * (window_w / CLASSIC_WINDOW_WIDTH))
         font_size = round(CLASSIC_WIDGETS_FONT_SIZE * (window_w / CLASSIC_WINDOW_WIDTH))
         table_font_size = (round(CLASSIC_TABLE_FONT_SIZE * (window_w / CLASSIC_WINDOW_WIDTH)))
 
         self.__found_frame = self.__create_found_frame(
-            button_w=scrollbar_frame_width // 6,
+            button_w=table_width // 6,
             button_h=window_h // 20,
-            entry_w=scrollbar_frame_width // 6 * 5,
+            entry_w=table_width // 6 * 5,
             entry_h=window_h // 20,
             font_size=font_size
         )
 
         self.__creating_frame = self.__create_creation_frame(
-            client_entry_w=round(60 * table_width_percentage),
-            article_entry_w=round(20 * table_width_percentage),
-            count_entry_w=round(20 * table_width_percentage),
+            client_entry_w=self.__table_column_width * 2,
+            article_entry_w=self.__table_column_width,
+            count_entry_w=self.__table_column_width,
             entryes_h=window_h // 20,
             font_size=font_size,
         )
 
         self.__crud_frame = self.__create_crud_frame(
-            button_w=scrollbar_frame_width // 3 - 20,
+            button_w=table_width // 3 - 20,
             button_h=window_h // 20,
             font_size=font_size
         )
 
-        self.__table_frame = ctk.CTkScrollableFrame(
-            master=self,
-            width=scrollbar_frame_width,
-            height=scrollbar_frame_height,
-            fg_color=self.cget("fg_color")
+        self.__table_frame = ctk.CTkFrame(master=self, fg_color="#313131", corner_radius=10)
+
+        self.__sales_table = Sheet(
+            self.__table_frame,
+            show_x_scrollbar=False,
+            show_y_scrollbar=False,
+
+            width=table_width,
+            height=table_height,
+            header_height=table_row_height,
+            row_height=table_row_height,
+
+            header_align="c",
+            align="c",
+
+            header_bg="#313131",
+            header_selected_cells_bg="#313131",
+            table_bg="#404040",
+
+            header_font=("Arial", table_font_size, "bold"),
+            header_selected_cells_fg="white",
+            header_fg="white",
+
+            font=("Arial", table_font_size, "normal"),
+            table_fg="white",
+
+            table_grid_fg="#313131",
+            header_grid_fg="#313131",
+
+            table_selected_rows_bg="#1E6AC4",
+            table_selected_rows_border_fg="#1E6AC4",
+            table_selected_rows_fg="white",
+
+            show_row_index=False,
+            show_top_left=False,
+
+            empty_vertical=False,
+            empty_horizontal=False
         )
 
-        self.__sales_table = CTkTable.CTkTable(
-            master=self.__table_frame,
-            values=[
-                ["Дата и время операции", "№ дисконтной карты клиента", "Артикул товара", "Кол-во"]],
-            row=12,
-            column=4,
-            height=cell_table_height,
-            font=("Arial", table_font_size)
-
+        self.__sales_table.headers(
+            [
+                "ID",
+                "Дата и время операции",
+                "Дисконтная карта клиента",
+                "Артикул товара",
+                "Кол-во",
+            ]
         )
 
-        self.__sales_table.edit_row(0, font=("Arial", font_size))
-        self.__sales_table.edit_column(0, width=round(30 * table_width_percentage))
-        self.__sales_table.edit_column(1, width=round(30 * table_width_percentage))
-        self.__sales_table.edit_column(2, width=round(20 * table_width_percentage))
-        self.__sales_table.edit_column(3, width=round(20 * table_width_percentage))
+        self.__sales_table.extra_bindings("cell_select", self.__table_row_selection)
+        self.__sales_table.enable_bindings("single_select")
+        self.__sales_table.hide_columns(columns=0)
 
         ctk.CTkLabel(
             master=self.__table_frame,
             text="Все операции",
             font=("Arial", font_size + 3),
-            anchor="center"
-        ).grid(row=0, column=0, padx=3, pady=3)
+            anchor="center",
+            text_color="white"
+        ).grid(row=0, column=0, padx=2)
 
-        self.__sales_table.grid(row=1, column=0, padx=3, pady=3)
+        self.__sales_table.grid(row=1, column=0, sticky="w", pady=5)
 
         ctk.CTkLabel(
             master=self,
             text="Продажи",
             font=("Arial", head_font_size)
-        ).grid(row=0, column=0, sticky="w", pady=3, padx=2)
+        ).grid(row=0, column=0, sticky="w", pady=2, padx=3)
 
-        self.__found_frame.grid(row=1, column=0, sticky="w", pady=3, padx=2)
-        self.__creating_frame.grid(row=2, column=0, sticky="w", pady=3, padx=2)
-        self.__table_frame.grid(row=3, column=0, pady=3, padx=2)
-        self.__crud_frame.grid(row=4, column=0, sticky="w", pady=3, padx=2)
+        self.__found_frame.grid(row=1, column=0, sticky="w", pady=2, padx=3)
+        self.__creating_frame.grid(row=2, column=0, sticky="w", pady=2, padx=3)
+        self.__table_frame.grid(row=3, column=0, pady=2, padx=3)
+        self.__crud_frame.grid(row=4, column=0, sticky="w", pady=2, padx=3)
+
+        self.bind("<Map>", self.__on_form_show_actions)
 
     def __create_creation_frame(
             self,
@@ -98,14 +134,14 @@ class SalesForm(ctk.CTkFrame):
     ) -> ctk.CTkFrame:
         creating_frame = ctk.CTkFrame(master=self, fg_color=self.cget("fg_color"))
 
-        self.__client_entry = ctk.CTkEntry(
+        self.__client_combobox = ctk.CTkComboBox(
             master=creating_frame,
             width=client_entry_w,
             height=entryes_h,
             font=("Arial", font_size)
         )
 
-        self.__article_entry = ctk.CTkEntry(
+        self.__article_combobox = ctk.CTkComboBox(
             master=creating_frame,
             width=article_entry_w,
             height=entryes_h,
@@ -116,12 +152,13 @@ class SalesForm(ctk.CTkFrame):
             master=creating_frame,
             width=count_entry_w,
             height=entryes_h,
-            font=("Arial", font_size)
+            font=("Arial", font_size),
+            placeholder_text="Кол-во:"
         )
 
-        self.__client_entry.grid(row=1, column=0, padx=2)
-        self.__article_entry.grid(row=1, column=1, padx=2)
-        self.__count_entry.grid(row=1, column=2, padx=2)
+        self.__client_combobox.grid(row=1, column=0)
+        self.__article_combobox.grid(row=1, column=1)
+        self.__count_entry.grid(row=1, column=2)
 
         ctk.CTkLabel(
             master=creating_frame,
@@ -155,10 +192,11 @@ class SalesForm(ctk.CTkFrame):
 
         self.__found_button = ctk.CTkButton(
             master=found_frame,
-            text="Поиск",
+            text="🔍",
             width=button_w,
             height=button_h,
-            font=("Arial", font_size)
+            font=("Arial", font_size + 10),
+            command=self.__find_sale
         )
 
         ctk.CTkLabel(
@@ -180,7 +218,8 @@ class SalesForm(ctk.CTkFrame):
             text="Добавить",
             width=button_w,
             height=button_h,
-            font=("Arial", font_size)
+            font=("Arial", font_size),
+            command=self.__add_sale
         )
 
         self.__del_button = ctk.CTkButton(
@@ -188,12 +227,133 @@ class SalesForm(ctk.CTkFrame):
             text="Удалить",
             width=button_w,
             height=button_h,
-            font=("Arial", font_size)
+            font=("Arial", font_size),
+            command=self.__del_sale
         )
 
         self.__add_button.grid(row=0, column=0, padx=2)
         self.__del_button.grid(row=0, column=1, padx=2)
 
         return crud_frame
+
+    def __clearing_entrys(self):
+        self.__count_entry.delete(0, ctk.END)
+        self.__article_combobox.set("")
+        self.__client_combobox.set("")
+        self.__count_entry._activate_placeholder()
+
+    def __table_row_selection(self, event):
+        selected_info = event["selected"]
+        self.__sales_table.select_row(selected_info.row)
+        *_, client_card, product_article, product_count = self.__sales_table.get_row_data(r=selected_info.row)
+        self.__clearing_entrys()
+        if client_card is None:
+            self.__client_combobox.set("")
+        else:
+            self.__client_combobox.set(client_card)
+        self.__article_combobox.set(product_article)
+        self.__count_entry.insert(0, product_count)
+
+    def __updating_table_data(self, new_data):
+        self.__sales_table.set_sheet_data(new_data)
+        self.__sales_table.deselect(row="all")
+        self.__sales_table.set_all_column_widths(self.__table_column_width)
+
+    def __on_form_show_actions(self, _):
+        self.__clearing_entrys()
+        self.__found_entry.delete(0, ctk.END)
+        table_data = list()
+        products_articles = list()
+        client_cards = list()
+
+        try:
+            table_data = back.get_sales()
+            products_articles = get_products_articles()
+            client_cards = get_client_cards()
+        except mysql.connector.errors.InterfaceError:
+            InformationDialog(
+                self.master,
+                "Ошибка подключения к БД!",
+                "Проверьте подключение к сети интернет\nлибо обратитесь к техническому специалисту!")
+
+        self.__article_combobox.configure(values=products_articles)
+        self.__client_combobox.configure(values=client_cards)
+        self.__updating_table_data(table_data)
+
+    def __add_sale(self):
+        date = datetime.now().replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
+        client_card = self.__client_combobox.get()
+        product_article = self.__article_combobox.get()
+        product_count = self.__count_entry.get()
+        try:
+            operation_id = back.add_sale(date, client_card, product_article, product_count)
+            added_record = [operation_id, date, client_card, product_article, product_count]
+            self.__sales_table.insert_row(idx=0, row=added_record, redraw=True)
+            self.__clearing_entrys()
+            self.__sales_table.deselect(row="all")
+        except mysql.connector.errors.InterfaceError:
+            InformationDialog(
+                self.master,
+                "Ошибка подключения к БД!",
+                "Проверьте подключение к сети интернет\nлибо обратитесь к техническому специалисту!")
+        except mysql.connector.errors.IntegrityError:
+            InformationDialog(
+                self.master,
+                "Ошибка данных!",
+                "Во время вышего сеанса критически важные данные были изменены!\nПерезайдите в текущий раздел для обновления данных.")
+        except TypeError as current_error:
+            if current_error.args[0] == "Incorrect article":
+                info = "Некорректный формат артикула. Его длинна должна быть\nне менее 1 и не более 10 цифр!"
+            elif current_error.args[0] == "Incorrect count":
+                info = "Некорректное кол-во товаров. Оно должно являться\nцелым положительным числом!"
+            elif current_error.args[0] == "Incorrect card":
+                info = "Некорректный формат номера карты. Он должен состоять из\n10 цифр!"
+            elif current_error.args[0] == "Article doesnt exist":
+                info = "Некорректный артикул. Товар с данным артикулом отсутствует\nв базе данных!"
+            elif current_error.args[0] == "Client doesnt exist":
+                info = "Некорректный номер карты. Клиент с данным номером отсутствует\nв базе данных!"
+            elif current_error.args[0] == "Product count is very big":
+                info = "Некорректное количество купленного товара. Оно не должно\nпревышать текущее количество товара!"
+            else:
+                info = "Непредвиденная ошибка :("
+            InformationDialog(self.master, "Некорректный ввод!", info)
+
+    def __del_sale(self):
+        selected_table_row = self.__sales_table.get_selected_rows(return_tuple=True)
+        if not selected_table_row:
+            InformationDialog(
+                self.master,
+                "Ошибка!",
+                "Ни одна строка таблицы не выбрана для удаления!")
+            return 0
+        selected_row = selected_table_row[0]
+        write_off_id = self.__sales_table.get_row_data(r=selected_row)[0]
+        dialog = ModalDialog(
+            self.master,
+            "Подтвердите действие.",
+            f"Вы действительно хотите удалить выбранную в таблице запись?\nУдаление данной записи не возвратит изменения\nв количестве товара."
+        )
+        dialog.wait_window()
+        if dialog.modal_result:
+            try:
+                back.del_sale(write_off_id)
+                self.__sales_table.delete_row(selected_row, redraw=True)
+                self.__clearing_entrys()
+                self.__sales_table.deselect(row="all")
+            except mysql.connector.errors.InterfaceError:
+                InformationDialog(
+                    self.master,
+                    "Ошибка подключения к БД!",
+                    "Проверьте подключение к сети интернет\nлибо обратитесь к техническому специалисту!")
+
+    def __find_sale(self):
+        try:
+            finding_record = back.get_finding_sales(self.__found_entry.get())
+            self.__updating_table_data(finding_record)
+        except mysql.connector.errors.InterfaceError:
+            InformationDialog(
+                self.master,
+                "Ошибка подключения к БД!",
+                "Проверьте подключение к сети интернет\nлибо обратитесь к техническому специалисту!")
 
 
